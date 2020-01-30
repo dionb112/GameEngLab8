a6f4402a6750d3278e9249526c8f0a51623e7110
dionb112 <dion-b@hotmail.com>
2020-01-30 21:08:12 +0000

    Final draft 1: 4 threads, CS is player pos, NEXT!

--- ConcurrentTicketSystem/main.cpp
+++ ConcurrentTicketSystem/main.cpp
@@ -2,18 +2,42 @@
 #include <mutex>
 #include <thread>
 #include <atomic>
+#include <SFML/Graphics.hpp>
+#include <string>
 
+static const int N = 15;
 std::mutex mutex;
-std::atomic_int _turn[];
+std::atomic_int turn[N] = { 0, 0, 0, 0, 0,0, 0, 0, 0, 0,0, 0, 0, 0, 0 };
+std::atomic_int num = 1;
+std::atomic_int next = 1;
+int threadCount = 0;
+sf::Vector2f playerPosition(42, 42);
+
 
 void ticket() {
+	int i = threadCount++;
 	mutex.lock();
 	std::cout << "Thread ID: " << std::this_thread::get_id() << std::endl;
 	mutex.unlock();
 	std::this_thread::sleep_for(std::chrono::seconds(1));
 
 	while (1) {
-
+		turn[i] = num.fetch_add(1);
+		mutex.lock();
+		std::cout << "Thread ID: " << std::this_thread::get_id() << "'s turn, order: " << turn[i] << std::endl;
+		mutex.unlock();
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+		while (turn[i] != next) {
+			continue;
+		}
+		mutex.lock();
+		std::cout << "Thread ID: " << std::this_thread::get_id() << "'s Critical Section " << turn[i] << std::endl;
+		mutex.unlock();
+		std::string data = "Player position: " + std::to_string(playerPosition.x) + ", " + std::to_string(playerPosition.y);
+		next += 1;
+		mutex.lock();
+		std::cout << data << " for Thread ID:" << std::this_thread::get_id() << ", next: " << next << std::endl;
+		mutex.unlock();
 	}
 
 }
